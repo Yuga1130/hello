c36ecf58a2e051d50307a427e92b89185558051e
Yuga Shigeyoshi <shigeyoshi@techfun-myanmar.com>
2019-11-29 16:44:56 +0630

    add

---
+++ hello-world/src/main/java/hello/HelloPrint.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloPrint {
+
+	public static void print() {
+		System.out.println("Hello!!!");
+	}
+}
---
+++ hello-world/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloWorld extends HelloPrint{
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven World!");
+
+		int i = 2;
+		if(i == 1) {
+			print();
+		}
+
+	}
+
+}
